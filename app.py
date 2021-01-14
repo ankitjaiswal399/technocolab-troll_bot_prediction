515869af390c14ce70274d960792ee4205674398
ankitjaiswal399 <ankitjaiswal399@gmail.com>
2021-01-14 22:48:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def main():
         <style> body { 
                 background-image: url("https://images.unsplash.com/photo-1531746790731-6c087fecd65a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1895&q=80"); 
                 background-size: cover; 
-                color : white;
+                color : black;
                 } 
                 </style> '''
 
